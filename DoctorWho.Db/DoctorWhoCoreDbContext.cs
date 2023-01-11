7ac95a1e7b11533110816fa6d14d74ad3ba5d16b
jihadabuzuhri <jihadabuzuhri@gmail.com>
2023-01-12 01:20:32 +0200

    Add DbContext

---
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoCoreDbContext :DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+
+    }
+}

e6a9fe343efe34f02bf60af7b7c2d9a3ee53bfd6
jihadabuzuhri <jihadabuzuhri@gmail.com>
2023-01-12 02:43:52 +0200

    Create DoctorWhoCoreDb With Migrations

--- DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -4,10 +4,53 @@ namespace DoctorWho.Db
 {
     public class DoctorWhoCoreDbContext :DbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Companion> Companions { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<EpisodeCompanion> EpisodeCompanions { get; set; }
+        public DbSet<EpisodeEnemy> EpisodeEnemies { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=DoctorWhoCore;Trusted_Connection=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasKey(a => a.AuthorId);
+            modelBuilder.Entity<Author>().Property(a => a.AuthorName).IsRequired();
+            modelBuilder.Entity<Author>().Property(a => a.AuthorName).HasColumnType("varchar(400)");
+
+            modelBuilder.Entity<Companion>().HasKey(c => c.CompanionId);
+            modelBuilder.Entity<Companion>().Property(c => c.CompanionName).IsRequired();
+            modelBuilder.Entity<Companion>().Property(c => c.CompanionName).HasColumnType("varchar(400)");
+
+            modelBuilder.Entity<Doctor>().HasKey(d => d.DoctorId);
+            modelBuilder.Entity<Doctor>().Property(d => d.DoctorName).IsRequired();
+            modelBuilder.Entity<Doctor>().Property(d => d.DoctorName).HasColumnType("varchar(400)");
+            modelBuilder.Entity<Doctor>().Property(d => d.BirthDate).HasDefaultValueSql("NULL");
+            modelBuilder.Entity<Doctor>().Property(d => d.FirstEpisodeDate).HasDefaultValueSql("NULL");
+            modelBuilder.Entity<Doctor>().Property(d => d.LastEpisodeDate).HasDefaultValueSql("NULL");
+
+            modelBuilder.Entity<Enemy>().HasKey(e => e.EnemyId);
+            modelBuilder.Entity<Enemy>().Property(e => e.EnemyName).IsRequired();
+            modelBuilder.Entity<Enemy>().Property(e => e.EnemyName).HasColumnType("varchar(400)");
+            modelBuilder.Entity<Enemy>().Property(e => e.Description).HasDefaultValueSql("NULL");
+
+            modelBuilder.Entity<Episode>().HasKey(e => e.EpisodeId);
+            modelBuilder.Entity<Episode>().Property(e => e.SeriesNumber).HasDefaultValueSql("0");
+            modelBuilder.Entity<Episode>().Property(e => e.EpisodeNumber).HasDefaultValueSql("0");
+            modelBuilder.Entity<Episode>().Property(e => e.EpisodeType).IsRequired();
+            modelBuilder.Entity<Episode>().Property(e => e.EpisodeType).HasColumnType("varchar(400)");
+            modelBuilder.Entity<Episode>().Property(e => e.Title).HasColumnType("varchar(400)");
+            modelBuilder.Entity<Episode>().Property(e => e.EpisodeDate).HasDefaultValueSql("NULL");
+            modelBuilder.Entity<Episode>().Property(e => e.Notes).HasColumnType("varchar(400)");
+            modelBuilder.Entity<Episode>().Property(e => e.Notes).HasDefaultValueSql("NULL");
         }
 
     }
